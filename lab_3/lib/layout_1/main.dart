e2113b6c2433a80a52407dbff42ebd38078ed9a9
KhangNguyen0105 <khangnguyen.010598@gmail.com>
2025-09-24 21:45:42 +0700

    Lab 3 - Layout 1

---
+++ lab_3/lib/layout_1/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:lab_3/layout_1/ButtonSection.dart';
+import 'package:lab_3/layout_1/ImageSection.dart';
+import 'package:lab_3/layout_1/TextSection.dart';
+import 'package:lab_3/layout_1/TitleSection.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Flutter layout demo';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(appTitle)),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageSection(image: 'assets/images/lake.png'),
+              TitleSection(
+                name: "Oeschinen Lake Campground",
+                location: "Kandersteg, Switzerland",
+              ),
+              ButtonSection(),
+              TextSection(
+                description:
+                    'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                    'Bernese Alps. Situated 1,578 meters above sea level, it '
+                    'is one of the larger Alpine Lakes. A gondola ride from '
+                    'Kandersteg, followed by a half-hour walk through pastures '
+                    'and pine forest, leads you to the lake, which warms to 20 '
+                    'degrees Celsius in the summer. Activities enjoyed here '
+                    'include rowing, and riding the summer toboggan run.',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
